634470aa8b907a19fe60accfbeb4497d676f5f6a
Nawazish Ali <nawazishaliejaz@gmail.com>
2017-06-28 00:54:34 +0500

    Table is now sortable, Idea fetched from Official VueJs2 Grid component.  Filteration to be implemented now

--- app/js-app/app.js
+++ app/js-app/app.js
@@ -47,9 +47,11 @@ Vue.component('tool-app', {
         return {
             heading: "Tools Management",
             tools: DB.tools.find(),
-            columns: ['partnumber', 'description', 'location', 'abqsno', 'calibdt', 'duedt'],
+            DB: "tools",
+            columns: ['partnumber', 'description', 'ABQ SNo.', 'location', 'Calibration Date', 'Due Date'],
             mode: "",
             editIndex: null,
+            id: ""
         }
     },
     methods: {
@@ -58,15 +60,15 @@ Vue.component('tool-app', {
             let obj = {
                 partnumber: toolsForm.partnumber.value,
                 description: toolsForm.description.value,
+                'ABQ SNo.': toolsForm.abqsno.value,
                 location: toolsForm.location.value,
-                abqsno: toolsForm.abqsno.value,
-                calibdt: toolsForm.calibdt.value,
-                duedt: toolsForm.duedt.value,
+                'Calibration Date': toolsForm.calibdt.value,
+                'Due Date': toolsForm.duedt.value,
                 historyArr: [{
-                    updateDt: formatDate(new Date()),
+                    'Date Updated': formatDate(new Date()),
                     location: toolsForm.location.value,
-                    calibdt: toolsForm.calibdt.value,
-                    duedt: toolsForm.duedt.value
+                    'Calibration Date': toolsForm.calibdt.value,
+                    'Due Date': toolsForm.duedt.value
                 }]
             };
             DB.tools.save(obj);
@@ -79,65 +81,30 @@ Vue.component('tool-app', {
                 });
             })
         },
-        deleteTool: function (index) {
-            console.log(index);
-            let confirmDelete = confirm("Are you sure you want to delete " + this.tools[index].description + " with SN: " + this.tools[index].abqsno);
-            if (confirmDelete === true) {
-                DB.tools.remove({
-                    _id: this.tools[index]._id
-                });
-                this.tools = DB.tools.find();
-                $('.tooltipped').tooltip('remove');
-            }
-            this.$nextTick(function () {
-                $('.datepicker').pickadate({
-                    closeOnSelect: true,
-                    selectMonths: true, // Creates a dropdown to control month
-                    selectYears: 15 // Creates a dropdown of 15 years to control year
-                });
-                $('.tooltipped').tooltip({
-                    delay: 50
-                });
-                Materialize.updateTextFields();
+        editTool: function () {
+            let toolsForm = document.getElementById('tools-form');
+            let tool = DB.tools.findOne({
+                _id: this.id
             });
-        },
-        initiateEditForm: function (index) {
-            $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
-            this.mode = "edit";
-            this.editIndex = index;
-            this.$nextTick(function () {
-                $('.datepicker').pickadate({
-                    closeOnSelect: true,
-                    selectMonths: true, // Creates a dropdown to control month
-                    selectYears: 15 // Creates a dropdown of 15 years to control year
-                });
-                $('.tooltipped').tooltip({
-                    delay: 50
-                });
-                Materialize.updateTextFields();
-            });
-        },
-        editTool: function (index) {
-            let toolsForm = document.getElementById('tools-form')
-            this.tools[index].partnumber = toolsForm.partnumber.value;
-            this.tools[index].description = toolsForm.description.value;
-            this.tools[index].abqsno = toolsForm.abqsno.value;
-            this.tools[index].location = toolsForm.location.value;
-            this.tools[index].calibdt = toolsForm.calibdt.value;
-            this.tools[index].duedt = toolsForm.duedt.value;
-            this.tools[index].historyArr[(this.tools[index].historyArr.length) - 1].location = toolsForm.location.value;
-            this.tools[index].historyArr[(this.tools[index].historyArr.length) - 1].calibdt = toolsForm.calibdt.value;
-            this.tools[index].historyArr[(this.tools[index].historyArr.length) - 1].duedt = toolsForm.duedt.value;
+            tool.partnumber = toolsForm.partnumber.value;
+            tool.description = toolsForm.description.value;
+            tool['ABQ SNo.'] = toolsForm.abqsno.value;
+            tool.location = toolsForm.location.value;
+            tool['Calibration Date'] = toolsForm.calibdt.value;
+            tool['Due Date'] = toolsForm.duedt.value;
+            tool.historyArr[(this.tools[index].historyArr.length) - 1].location = toolsForm.location.value;
+            tool.historyArr[(this.tools[index].historyArr.length) - 1]['Calibration Date'] = toolsForm.calibdt.value;
+            tool.historyArr[(this.tools[index].historyArr.length) - 1]['Due Date'] = toolsForm.duedt.value;
             DB.tools.update({
-                _id: this.tools[index]._id
-            }, this.tools[index], {
+                _id: this.id
+            }, tool, {
                 multi: false,
                 upsert: false
             });
             this.tools = DB.tools.find();
             $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
             this.mode = "";
-            this.editIndex = null;
+            this.id = "";
             this.$nextTick(function () {
                 $('.datepicker').pickadate({
                     closeOnSelect: true,
@@ -150,37 +117,24 @@ Vue.component('tool-app', {
                 });
             })
         },
-        initiateUpdateForm: function (index) {
-            $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
-            this.mode = "update";
-            this.editIndex = index;
-            this.$nextTick(function () {
-                $('.datepicker').pickadate({
-                    closeOnSelect: true,
-                    selectMonths: true, // Creates a dropdown to control month
-                    selectYears: 15 // Creates a dropdown of 15 years to control year
-                });
-                $('.tooltipped').tooltip({
-                    delay: 50
-                });
-                Materialize.updateTextFields();
-            })
-        },
-        updateTool: function (index) {
+        updateTool: function () {
             let toolsForm = document.getElementById('tools-form');
-            this.tools[index].location = toolsForm.location.value;
-            this.tools[index].calibdt = toolsForm.calibdt.value;
-            this.tools[index].duedt = toolsForm.duedt.value;
+            let tool = DB.tools.findOne({
+                _id: this.id
+            });
+            tool.location = toolsForm.location.value;
+            tool['Calibration Date'] = toolsForm.calibdt.value;
+            tool['Due Date'] = toolsForm.duedt.value;
             let history = {
-                updateDt: formatDate(new Date()),
-                location: this.tools[index].location,
-                calibdt: this.tools[index].calibdt,
-                duedt: this.tools[index].duedt
+                'Date Updated': formatDate(new Date()),
+                location: tool.location,
+                'Calibration Date': tool['Calibration Date'],
+                'Due Date': tool['Due Date']
             };
-            this.tools[index].historyArr.push(history);
+            tool.historyArr.push(history);
             DB.tools.update({
-                _id: this.tools[index]._id
-            }, this.tools[index], {
+                _id: this.id
+            }, tool, {
                 multi: false,
                 upsert: false
             });
@@ -188,7 +142,7 @@ Vue.component('tool-app', {
 
             $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
             this.mode = "";
-            this.editIndex = null;
+            this.id = "";
             this.$nextTick(function () {
                 $('.datepicker').pickadate({
                     closeOnSelect: true,
@@ -201,16 +155,36 @@ Vue.component('tool-app', {
                 });
             })
         },
-        initiateView: function (index) {
-            this.editIndex = index;
+        deleteTool: function () {
+            let tool = DB.tools.findOne({
+                _id: this.id
+            });
+            let confirmDelete = confirm("Are you sure you want to delete " + tool.description + " with SN: " + tool['ABQ SNo.']);
+            if (confirmDelete === true) {
+                DB.tools.remove({
+                    _id: this.id
+                });
+                $('.tooltipped').tooltip('remove');
+            };
+            this.tools = DB.tools.find();
+            this.mode = "";
+            this.id = "";
             this.$nextTick(function () {
-                $('.modal').modal();
-                $('#modal1').modal('open');
+                $('.datepicker').pickadate({
+                    closeOnSelect: true,
+                    selectMonths: true, // Creates a dropdown to control month
+                    selectYears: 15 // Creates a dropdown of 15 years to control year
+                });
+                $('.tooltipped').tooltip({
+                    delay: 50
+                });
+                Materialize.updateTextFields();
             });
         },
         cancel: function () {
             this.mode = "";
             this.editIndex = null;
+            this.id = "";
             $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
             this.$nextTick(function () {
                 $('.datepicker').pickadate({
@@ -224,6 +198,15 @@ Vue.component('tool-app', {
                 });
                 Materialize.updateTextFields();
             })
+        },
+        updateParent: function (data) {
+            this.tools = data.updateDB;
+            this.mode = data.updateMode;
+            this.editIndex = data.updateIndex;
+            this.id = data.updateId;
+            if(this.mode === "delete"){
+                this.deleteTool();
+            }
         }
 
     }
@@ -232,57 +215,124 @@ Vue.component('hello-world', {
     template: '<h1>Hello World </h1>'
 });
 // register the grid component
-Vue.component('demo-grid', {
-  template: '#grid-template',
-  props: {
-    data: Array,
-    columns: Array,
-    filterKey: String
-  },
-  data: function () {
-    var sortOrders = {}
-    this.columns.forEach(function (key) {
-      sortOrders[key] = 1
-    })
-    return {
-      sortKey: '',
-      sortOrders: sortOrders
-    }
-  },
-  computed: {
-    filteredData: function () {
-      var sortKey = this.sortKey
-      var filterKey = this.filterKey && this.filterKey.toLowerCase()
-      var order = this.sortOrders[sortKey] || 1
-      var data = this.data
-      if (filterKey) {
-        data = data.filter(function (row) {
-          return Object.keys(row).some(function (key) {
-            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-          })
+Vue.component('tool-table', {
+    template: '#table-template',
+    props: {
+        data: Array,
+        db: String,
+        id: String,
+        columns: Array,
+        filterKey: String,
+        mode: String
+    },
+    data: function () {
+        var sortOrders = {}
+        this.columns.forEach(function (key) {
+            sortOrders[key] = 1
         })
-      }
-      if (sortKey) {
-        data = data.slice().sort(function (a, b) {
-          a = a[sortKey]
-          b = b[sortKey]
-          return (a === b ? 0 : a > b ? 1 : -1) * order
-        })
-      }
-      return data
+        return {
+            sortKey: '',
+            sortOrders: sortOrders
+        }
+    },
+    computed: {
+        filteredData: function () {
+            var sortKey = this.sortKey
+            var filterKey = this.filterKey && this.filterKey.toLowerCase()
+            var order = this.sortOrders[sortKey] || 1
+            var data = this.data
+            if (filterKey) {
+                data = data.filter(function (row) {
+                    return Object.keys(row).some(function (key) {
+                        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+                    })
+                })
+            }
+            if (sortKey) {
+                data = data.sort(function (a, b) {
+                    a = a[sortKey]
+                    b = b[sortKey]
+                    return (a === b ? 0 : a > b ? 1 : -1) * order
+                })
+            }
+            return data;
+        }
+    },
+    filters: {
+        capitalize: function (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+        }
+    },
+    methods: {
+        sortBy: function (key) {
+            this.sortKey = key
+            this.sortOrders[key] = this.sortOrders[key] * -1
+        },
+        initiateDelete: function (index) {
+            this.$emit('updateparent', {
+                updateDB: DB[this.db].find(),
+                updateMode: "delete",
+                updateId: this.data[index]._id,
+                updateIndex: null
+            });
+        },
+        initiateEditForm: function (index) {
+            $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
+            this.$emit('updateparent', {
+                updateDB: DB[this.db].find(),
+                updateMode: "edit",
+                updateId: this.data[index]._id,
+                updateIndex: index
+            });
+            this.$nextTick(function () {
+                $('.datepicker').pickadate({
+                    closeOnSelect: true,
+                    selectMonths: true, // Creates a dropdown to control month
+                    selectYears: 15 // Creates a dropdown of 15 years to control year
+                });
+                $('.tooltipped').tooltip({
+                    delay: 50
+                });
+                Materialize.updateTextFields();
+            });
+        },
+        initiateUpdateForm: function (index) {
+            $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
+            this.$emit('updateparent', {
+                updateDB: DB[this.db].find(),
+                updateMode: "update",
+                updateId: this.data[index]._id,
+                updateIndex: index
+            });
+            this.$nextTick(function () {
+                $('.datepicker').pickadate({
+                    closeOnSelect: true,
+                    selectMonths: true, // Creates a dropdown to control month
+                    selectYears: 15 // Creates a dropdown of 15 years to control year
+                });
+                $('.tooltipped').tooltip({
+                    delay: 50
+                });
+                Materialize.updateTextFields();
+            })
+        },
+        initiateView: function (index) {
+            $('.tooltipped').tooltip('remove'); //Tooltip kept showing constatly after the form was replaced, so removed it.
+            this.$emit('updateparent', {
+                updateDB: DB[this.db].find(),
+                updateMode: "",
+                updateId: this.data[index]._id,
+                updateIndex: index
+            });
+            this.$nextTick(function () {
+                $('.tooltipped').tooltip({
+                    delay: 50
+                });
+                $('.modal').modal();
+                $('#modal1').modal('open');
+            });
+        }
     }
-  },
-  filters: {
-    capitalize: function (str) {
-      return str.charAt(0).toUpperCase() + str.slice(1)
-    }
-  },
-  methods: {
-    sortBy: function (key) {
-      this.sortKey = key
-      this.sortOrders[key] = this.sortOrders[key] * -1
-    }
-  }
 })
 
 
